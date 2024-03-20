7829846b7718a8867569423d51121b8f020beb4a
paulcaruana <paul.caruana@objective.com>
2024-03-20 13:18:38 +1100

    Transition part 2

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,4 +1,48 @@
 import { initTRPC } from "@trpc/server";
+import { db } from "@/db";
+
+/**
+ * 1. CONTEXT
+ *
+ * This section defines the "contexts" that are available in the backend API.
+ *
+ * These allow you to access things when processing a request, like the database, the session, etc.
+ */
+
+interface CreateContextOptions {
+  headers: Headers;
+}
+
+/**
+ * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
+ * it from here.
+ *
+ * Examples of things you may need it for:
+ * - testing, so we don't have to mock Next.js' req/res
+ * - tRPC's `createSSGHelpers`, where we don't have req/res
+ *
+ * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
+ */
+export const createInnerTRPCContext = (opts: CreateContextOptions) => {
+  return {
+    headers: opts.headers,
+    db,
+  };
+};
+
+/**
+ * This is the actual context you will use in your router. It will be used to process every request
+ * that goes through your tRPC endpoint.
+ *
+ * @see https://trpc.io/docs/context
+ */
+export const createTRPCContext = (opts: { req: Request }) => {
+  // Fetch stuff that depends on the request
+
+  return createInnerTRPCContext({
+    headers: opts.req.headers,
+  });
+};
 
 const t = initTRPC.create();
 
