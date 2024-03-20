87027db562b7ee07b2f19c1325ea878939dd620e
paulcaruana <paul.caruana@objective.com>
2024-03-20 12:54:47 +1100

    Transition part 1

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,15 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
-
+import { sql } from "drizzle-orm";
 export const todos = sqliteTable("todos", {
   id: integer("id").primaryKey(),
   content: text("content"),
   done: integer("done"),
+});
+
+export const user = sqliteTable("user", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  firstName: text("first_name", { length: 256 }),
+  lastName: text("last_name", { length: 256 }),
+  username: text("user_name").unique().notNull(),
+  createdAt: text("created_at").default(sql`CURRENT_TIME`),
 });
